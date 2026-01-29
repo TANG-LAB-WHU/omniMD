71560a9ca100d1fa48707b15358fee50b91fd21f
ResearchGeekSQ <siqit@outlook.com>
2026-01-29 14:49:15 +0800

    feat: Add NIST LJ and SPC/E test data and a new potential tutorial.

---
+++ tests/utils/mod.rs
@@ -0,0 +1,55 @@
+﻿// Lumol, an extensible molecular simulation engine
+// Copyright (C) Lumol's contributors — BSD license
+#![allow(dead_code)]
+
+use omnimd::sim::output::Output;
+use omnimd::System;
+
+use std::rc::Rc;
+use std::sync::RwLock;
+
+pub type SharedVec = Rc<RwLock<Vec<f64>>>;
+
+/// Collect pressure and temperature of a simulation after a starting step
+pub struct Collector {
+    start: u64,
+    pressures: SharedVec,
+    temperatures: SharedVec,
+}
+
+impl Collector {
+    pub fn starting_at(start: u64) -> Collector {
+        let pressures = Vec::with_capacity(10_000);
+        let temperatures = Vec::with_capacity(10_000);
+        Collector {
+            start,
+            pressures: Rc::new(RwLock::new(pressures)),
+            temperatures: Rc::new(RwLock::new(temperatures)),
+        }
+    }
+
+    pub fn temperatures(&self) -> SharedVec {
+        self.temperatures.clone()
+    }
+
+    pub fn pressures(&self) -> SharedVec {
+        self.pressures.clone()
+    }
+}
+
+impl Output for Collector {
+    fn write(&mut self, system: &System) {
+        if system.step < self.start {
+            return;
+        }
+
+        self.pressures.write().unwrap().push(system.pressure());
+        self.temperatures.write().unwrap().push(system.temperature());
+    }
+}
+
+pub fn mean(data: SharedVec) -> f64 {
+    let data = data.read().unwrap();
+    data.iter().sum::<f64>() / data.len() as f64
+}
+
---
+++ tutorials/potential/src/main.rs
@@ -0,0 +1,38 @@
+﻿use lumol_tutorial_potential::Mie;
+
+use omnimd::input::Input;
+use omnimd::energy::PairInteraction;
+use omnimd::units;
+
+// This function runs a small Monte-Carlo simulation for Argon using a Mie potential.
+fn main() {
+    // read configuration from input file located at the data/ folder
+    let config = Input::new("data/argon.toml").unwrap().read().unwrap();
+    let mut system = config.system;
+    let mut simulation = config.simulation;
+
+    // build a potential
+    let mie = Mie::new(units::from(3.405, "A").unwrap(),
+                       units::from(1.0, "kJ/mol").unwrap(),
+                       12.0,
+                       6.0);
+    // use the potential with a cut off radius of 3 * sigma
+    let mut interaction = PairInteraction::new(Box::new(mie), units::from(3.0 * 3.405, "A").unwrap());
+    // use tail corrections to account for our truncation
+    interaction.enable_tail_corrections();
+    // finally use this interaction for Argon atoms
+    system.set_pair_potential(("Ar", "Ar"), interaction);
+
+    // report the initial system energy
+    let initial_energy = units::to(system.total_energy(), "kJ/mol").unwrap();
+    println!("initial energy     : {}", initial_energy);
+
+    // run the simulation
+    simulation.run(&mut system, config.nsteps);
+
+    // print some final information
+    let final_energy = units::to(system.total_energy(), "kJ/mol").unwrap();
+    println!("final energy       : {}", final_energy);
+    println!("It worked. Hooray!")
+}
+
