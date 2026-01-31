a7603ceeecf016c507356311632ce7bc331c8175
ResearchGeekSQ <siqit@outlook.com>
2026-01-31 11:09:19 +0800

    feat: Add tests for sodium chloride crystal properties using Wolf and Ewald summation methods.

--- tests/md-nacl.rs
+++ tests/md-nacl.rs
@@ -16,8 +16,8 @@ mod wolf {
     #[test]
     fn constant_energy() {
         if crate::utils::is_ci() {
-        return;
-    }
+            return;
+        }
         START.call_once(::env_logger::init);
         let path = Path::new(file!())
             .parent()
@@ -37,8 +37,8 @@ mod wolf {
     #[test]
     fn anisotropic_berendsen() {
         if crate::utils::is_ci() {
-        return;
-    }
+            return;
+        }
         START.call_once(::env_logger::init);
         let path = Path::new(file!())
             .parent()
@@ -73,8 +73,8 @@ mod ewald {
     #[test]
     fn constant_energy() {
         if crate::utils::is_ci() {
-        return;
-    }
+            return;
+        }
         START.call_once(::env_logger::init);
         let path = Path::new(file!())
             .parent()
@@ -93,8 +93,8 @@ mod ewald {
     #[test]
     fn constant_energy_kspace() {
         if crate::utils::is_ci() {
-        return;
-    }
+            return;
+        }
         START.call_once(::env_logger::init);
         let path = Path::new(file!())
             .parent()
